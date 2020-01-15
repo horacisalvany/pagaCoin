d951fd14503abcf4b2f7db3dc0f8b20bea2fb186
horacisalvany <horacimail@gmail.com>
2020-01-15 19:43:34 +0100

    Added a new button to cancel last created transfer

--- frontend/src/app/wallet-list/wallet-list.component.ts
+++ frontend/src/app/wallet-list/wallet-list.component.ts
@@ -25,7 +25,8 @@ export class WalletListComponent implements OnInit {
   selectedSourceOption: number;
   selectedDestinationOption: number;
   amount = 0;
-  transCreated = false;
+  transferCreated = false;
+  disableAllInputs = false;
   @ViewChild('f') form: NgForm;
   @ViewChild('alertWarning', { read: ElementRef }) alertWarning: ElementRef;
   @ViewChild('alertSuccess', { read: ElementRef }) alertSuccess: ElementRef;
@@ -46,7 +47,8 @@ export class WalletListComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.transCreated === false) {
+    if (this.transferCreated === false) {
+      this.disableAllInputs = true;
       this.alertWarning.nativeElement.hidden = true;
       const walletSource = this.wallets.find (i => i.id == this.selectedSourceOption);
       const walletDestination = this.wallets.find (i => i.id == this.selectedDestinationOption);
@@ -62,24 +64,26 @@ export class WalletListComponent implements OnInit {
         if (data && data.id != null) {
           this.transfer = data;
           console.log(this.transfer);
-          this.transCreated = true;
+          this.transferCreated = true;
           this.alertInfo.nativeElement.hidden = false;
         }
       });
     } else {
       this.transfer.amount = this.amount;
-      this.transferService.commitTransfer(this.transfer).then( () =>  this.transCreated = false).
+      this.transferService.commitTransfer(this.transfer).then( () =>  this.transferCreated = false).
       then(()  => {
         this.alertWarning.nativeElement.hidden = true;
         this.alertInfo.nativeElement.hidden = true;
         this.alertSuccess.nativeElement.hidden = false;
+        this.disableAllInputs = false;
+        this.form.reset();
       })
       .catch(error => {
         this.alertWarning.nativeElement.hidden = false;
         this.alertSuccess.nativeElement.hidden = true;
         this.alertInfo.nativeElement.hidden = true;
         this.alertWarning.nativeElement.innerText = 'Warning! ' + error.error;
-        this.transCreated = false;
+        this.transferCreated = false;
       });
     }
   }
@@ -90,6 +94,13 @@ export class WalletListComponent implements OnInit {
     console.log(this.form.controls.amount.value);
     this.amount = this.form.controls.amount.value;
     this.walletsDestination.splice(this.findIndexWallet(this.wallets, this.selectedSourceOption), 1);
+  }
+
+  onClickCancelTransfer() {
+    this.form.reset();
+    this.alertInfo.nativeElement.hidden = true;
+    this.transferCreated = false;
+    this.disableAllInputs = false;
   }
 
   private findIndexWallet(wallets: Wallet[], index: number): number {
